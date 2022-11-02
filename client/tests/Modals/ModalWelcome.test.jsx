c56c9e6a8d9d48a846a93db33ed85a6f8e46a6fe
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-11-02 02:03:09 -0300

    test on modal welcome

--- client/tests/Modals/ModalWelcome.test.jsx
+++ client/tests/Modals/ModalWelcome.test.jsx
@@ -5,6 +5,7 @@ import {
 	renderHook,
 	act,
 } from '@testing-library/react';
+
 import { vi, expect, describe, it, beforeAll } from 'vitest';
 import { ModalWelcome } from './../../src/components/Modals/ModalWelcome';
 import { container } from '@testing-library/jest-dom';
@@ -15,12 +16,25 @@ describe('Test suite on ModalWelcome component', () => {
 		welcome_text_1: 'personal',
 		welcome_text_2: 'functionalities',
 		name: 'Pablo',
+		alt: 'PabloCarranza',
 	};
 
 	const isOpen = true;
 
-	it('must show the gretting text', () => {
-		const component = render(
+	it('must show the alt text on image render', () => {
+		render(
+			<ModalWelcome
+				{...ModalWelcomeDates}
+				onClose={() => {}}
+				isOpen={isOpen}
+				setIsloaded={() => {}}
+			/>
+		);
+		expect(screen.getByRole('img').alt).toBe(ModalWelcomeDates.alt);
+	});
+
+	it('should display name on component', () => {
+		render(
 			<ModalWelcome
 				{...ModalWelcomeDates}
 				onClose={() => {}}
@@ -29,7 +43,49 @@ describe('Test suite on ModalWelcome component', () => {
 			/>
 		);
 
-		screen.debug();
-		//expect(screen.getByText('functionalities'));
+		const text = screen.getByRole('dialog');
+		expect(text).toHaveTextContent(ModalWelcomeDates.name);
+	});
+
+	it('should display gretting on component', () => {
+		render(
+			<ModalWelcome
+				{...ModalWelcomeDates}
+				onClose={() => {}}
+				isOpen={isOpen}
+				setIsloaded={() => {}}
+			/>
+		);
+
+		const text = screen.getByRole('dialog');
+		expect(text).toHaveTextContent(ModalWelcomeDates.greeting);
+	});
+
+	it('should display welcome text one on component', () => {
+		render(
+			<ModalWelcome
+				{...ModalWelcomeDates}
+				onClose={() => {}}
+				isOpen={isOpen}
+				setIsloaded={() => {}}
+			/>
+		);
+
+		const text = screen.getByRole('dialog');
+		expect(text).toHaveTextContent(ModalWelcomeDates.welcome_text_1);
+	});
+
+	it('should display welcome text two on component', () => {
+		render(
+			<ModalWelcome
+				{...ModalWelcomeDates}
+				onClose={() => {}}
+				isOpen={isOpen}
+				setIsloaded={() => {}}
+			/>
+		);
+
+		const text = screen.getByRole('dialog');
+		expect(text).toHaveTextContent(ModalWelcomeDates.welcome_text_2);
 	});
 });
