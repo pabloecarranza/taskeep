a68252d968af0c1dc89cc05edb25ef59c002353a
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-11-13 03:23:15 -0300

    mejorando test en addtask

--- client/tests/src/components/AddTask/AddTask.test.jsx
+++ client/tests/src/components/AddTask/AddTask.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { expect, describe, it, vi, beforeEach } from 'vitest';
+import { expect, describe, it, vi } from 'vitest';
 import { AddTask } from '../../../../src/components/AddTask/AddTask';
 import { AddTaskDates } from './../../../../src/utils/EnglishTexts';
 import { renderWithProviders } from './../../../../src/utils/utils-for-test';
@@ -7,55 +7,41 @@ import { useAddTask } from './../../../../src/Hooks/useAddTask';
 
 vi.mock('../../../../src/Hooks/useAddTask');
 
-describe('Name of the group', () => {
-	it('should ', () => {
+describe('Test suite on AddTask component', () => {
+	//en esta prueba logre mockear un customHook
+
+	it('debe mostrar el texto del boton y el text de placeholder del input enviado por props ', () => {
 		const capitalizeFirstLetter = vi.fn();
 		const handleOnChange = vi.fn();
 		const handleSubmit = vi.fn();
 		useAddTask = vi.fn().mockReturnValue({
-			PostTaskResponse: {
-				isError: false,
-				isLoading: false,
-				isSuccess: false,
-				isUninitialized: true,
-				originalArgs: undefined,
-				reset: vi.fn(),
-				status: 'uninitialized',
-			},
+			PostTaskResponse: {},
 			capitalizeFirstLetter,
 			handleOnChange,
 			handleSubmit,
-			data: [
-				{
-					createdAt: '2022-11-13T03:41:25.984Z',
-					id: 110,
-					name: 'a',
-					updatedAt: '2022-11-13T03:41:25.984Z',
-				},
-			],
-			task: {
-				completed: false,
-				description: '',
-				expiration_date: '',
-				important: false,
-				listid: null,
-				notes: '',
-				reminder: 'YYYY-MM-DD',
-				repeat: 'YYYY-MM-DD',
-				userid: 'cc611eed-f679-45e0-b0ff-795e0ccbdbf1',
-			},
+			data: [],
+			task: {},
 			isLoading: false,
 		});
 		renderWithProviders(<AddTask {...AddTaskDates} />);
+
+		const buttonAdd = screen.getByText(AddTaskDates.button_text);
+		const inputTask = screen.getByPlaceholderText(
+			AddTaskDates.placeholder_text
+		);
+
+		expect(inputTask).toBeTruthy();
+		expect(buttonAdd.textContent).toEqual(AddTaskDates.button_text);
 	});
 });
 
 /*
-const userData = {
-email: "pabloecarranza@gmail.com",
-id: "cc611eed-f679-45e0-b0ff-795e0ccbdbf1",
-message: "Login successfully",
-token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNjNjExZWVkLWY2NzktNDVlMC1iMGZmLTc5NWUwY2NiZGJmMSIsImlhdCI6MTY2ODMxMDAxNiwiZXhwIjoxNjY4Mzk2NDE2fQ.Op97JOUgK6yzmFB6pwjXk5qcR3yN5DWJOAMWbnqeVX0",
-username: "a"
-}
+
+debe mostrar el spinner cuando se esta cargando la respuesta del posteo de la tarea
+
+debe ocultar el icono de la tarea cuando se esta cargando la respuesta del posteo de la tarea
+
+al cambiar el valor del input debe llamar a la funcion handleOnChange
+
+al hacer click en Add debe llamar a handleSubmit
 */
