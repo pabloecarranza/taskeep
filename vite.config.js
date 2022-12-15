aaf445aa121ea6e79c5152c20902293c28c2ee16
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-12-15 01:28:13 -0300

    fix src test runner

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: '../client/src/test/setup.ts',
+		setupFiles: '/src/test/setup.ts',
 		css: true,
 		coverage: {
 			provider: 'c8',
